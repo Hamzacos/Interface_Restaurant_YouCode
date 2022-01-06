6fc38a4e9e1cd347835baabbaeb5153c9b2ab037
abdenacer <nasseressaouira@gmail.com>
2022-01-06 10:21:31 +0100

    Page Contact Us Done et Responssive

--- creation de code/index.js
+++ creation de code/index.js
@@ -1,8 +1,9 @@
-
+// not a good practice but It works right now (testing stat)
       window.addEventListener("DOMContentLoaded", () => {
         let navIcons = document.querySelectorAll(".icon1 a")
         navIcons.forEach(icon => {
-          if(icon.href == window.location.href) {
+          console.log(icon.href)
+          if(icon.href == window.location.href + '#') {
             icon.classList.add("active")
           } else {
             icon.classList.remove("active")
